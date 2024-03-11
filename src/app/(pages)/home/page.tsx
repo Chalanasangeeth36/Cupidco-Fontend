985dbbb8a9ec34eebcd8ebabea006b74cebc9524
chalanasangeeth36 <chalanasthisera@gmail.com>
2024-03-11 20:51:16 +0530

    components updated

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -4,7 +4,6 @@ import HomeSection2 from "../_page-components/home/HomeSection2";
 export default function Home() {
   return (
     <>
-      <div className="text-2xl">Home Page</div>
       <HomeSection1 />
       <HomeSection2 />
     </>
